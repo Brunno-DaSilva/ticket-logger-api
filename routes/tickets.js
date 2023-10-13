c1e464e506e414f900b4034d133247c191dc84cf
Bruno.LeandrodaSilva <bruno.leandrodasilva@trace3.com>
2023-10-13 14:46:02 -0500

    Added ticket CRUD

--- routes/tickets.js
+++ routes/tickets.js
@@ -5,34 +5,68 @@ const Ticket = require("../models/ticket.js");
 // Get All
 
 router.get("/", async (req, res) => {
-  const ticket = new Ticket({
-    subject: req.body.subject,
-    description: req.body.description,
-    progress: req.body.progress,
-    category: req.body.category,
-    createdDate: req.body.createdDate,
-    closedDate: req.body.closedDate,
-  });
   try {
-    const newTicket = await ticket.save();
-    res.status(201).json(newTicket);
+    const tickets = await Ticket.find();
+    res.json(tickets);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
 // Get one
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", async (req, res) => {
+  try {
+    const ticket = await Ticket.findById(req.params.id);
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    res.json(ticket);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 // creating one
 
-router.post("/tickets", (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    const newTicket = new Ticket(req.body);
+    await newTicket.save();
+    res.json(newTicket);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 // Updating one
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedTicket = await Ticket.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!updatedTicket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    res.json(updatedTicket);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 // deleing one
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedTicket = await Ticket.findByIdAndRemove(req.params.id);
+    if (!deletedTicket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    res.json({ message: "Ticket deleted" });
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 module.exports = router;
